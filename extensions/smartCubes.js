30ed821caaacc17a92dd93adf1f9cd8213b0383a
mariocraft <154646419+mariocraft987@users.noreply.github.com>
2024-03-01 11:58:05 -0700

    Update smartCubes.js

--- extensions/smartCubes.js
+++ extensions/smartCubes.js
@@ -7,6 +7,25 @@
         name: 'Smart Cubes',
         blocks: [
           {
+            opcode: "comment",
+            blockType: Scratch.BlockType.COMMAND,
+            text: "//[micro]",
+            disableMonitor: true,
+            arguments: {
+              micro: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: "true"
+              }
+            }
+          },
+          {
+            opcode: "longComment",
+            blockType: Scratch.BlockType.CONDITIONAL,
+            text: "/*",
+            disableMonitor: true,
+          },
+          { blockType: Scratch.BlockType.LABEL, text: "AI Blocks" },
+          {
             opcode: "AInow",
             blockType: Scratch.BlockType.BOOLEAN,
             text: "Is CubeAI available?",
@@ -29,9 +48,19 @@
       };
     }
 
+    comment(args) {
+        // Comment 
+        // args.micro
+    }
+    longComment(args) {
+        /* Long Comment 
+        args.micro */
+    }
+    
     AInow(args) {
         return true
     }
+    
     AIdecide(args) {
       let number = Math.floor(Math.random() * 2);
       if (number === 1) {
@@ -40,6 +69,7 @@
         return false
       }
     }
+    
     AIcolor(args) {
       return "#000000".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16);})
     }
