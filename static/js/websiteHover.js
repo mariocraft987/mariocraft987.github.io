3703f85031ba2bfd87ba1175373d0089b9b64b9e
AtomicBolts <154646419+mariocraft987@users.noreply.github.com>
2024-08-16 12:31:25 -0700

    Update websiteHover.js

--- static/js/websiteHover.js
+++ static/js/websiteHover.js
@@ -1,2 +1,2 @@
-document.getElementById('m987weblink').addEventListener("mousehover", () => document.getElementById('m987weblink').src = "https://mariocraft987.github.io/static/hand.png");
+document.getElementById('m987weblink').addEventListener("mouseover", () => document.getElementById('m987weblink').src = "https://mariocraft987.github.io/static/hand.png");
 document.getElementById('m987weblink').addEventListener("mouseleave", () => document.getElementById('m987weblink').src = "https://mariocraft987.github.io/static/idle.png");
