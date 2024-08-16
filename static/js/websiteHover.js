8e74196e7df95e68b429f3d7aea2a8f9b5da4406
AtomicBolts <154646419+mariocraft987@users.noreply.github.com>
2024-08-16 12:02:03 -0700

    Create websiteHover.js

---
+++ static/js/websiteHover.js
@@ -0,0 +1,4 @@
+const websiteLink = document.getElementById('m987weblink');
+
+websiteLink.addEventListener("mouseleave", () => websiteLink.src = "https://mariocraft987.github.io/static/idle.png");
+websiteLink.addEventListener("mouseover", () => websiteLink.src = "https://mariocraft987.github.io/static/hand.png");
